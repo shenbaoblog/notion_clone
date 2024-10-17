c5444dfff93bcd94e560a2243e7a3fcd106cdcd6
shenbaoblog <shenbaoblog@gmail.com>
2024-10-18 02:27:27 +0900

    ローカルサーバーの立ち上げ

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
-console.log("test");
+const express = require("express");
+const app = express();
+const PORT = 5555;
+
+app.get("/", (req, res) => {
+  res.send("Hello Express!");
+});
+
+app.listen(PORT, () => {
+  console.log(`ロカルサーバー起動中・・・`);
+});
